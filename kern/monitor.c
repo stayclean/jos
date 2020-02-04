9c3385af031d955b3a612dcb727c5cf353ca5c51
stayclean <stayclean@localhost.localdomain>
2020-02-04 20:11:23 +0800

    Complete debuginfoeip.

--- kern/monitor.c
+++ kern/monitor.c
@@ -25,7 +25,7 @@ struct Command {
 static struct Command commands[] = {
 	{ "help", "Display this list of commands", mon_help },
 	{ "kerninfo", "Display information about the kernel", mon_kerninfo },
-	{ "trace","", mon_backtrace }
+	{ "backtrace","", mon_backtrace }
 };
 
 /***** Implementations of basic kernel monitor commands *****/
